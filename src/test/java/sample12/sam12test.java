88c82f3a1dd3dcdc1d97df923d2fc2b2f7992062
vamshi <vamshi.doc001@gmail.com>
2019-05-24 23:49:43 +0530

    this is sample 12

---
+++ src/test/java/sample12/sam12test.java
@@ -0,0 +1,19 @@
+package sample12;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+public class sam12test {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
